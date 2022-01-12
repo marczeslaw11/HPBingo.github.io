37009b3d3e06050c9b24caeb8474fafc33f73ff5
durnellm <50713265+durnellm@users.noreply.github.com>
2022-01-11 21:46:41 -0500

    Add files via upload

--- FS v1.0/goal-list.js
+++ FS v1.0/goal-list.js
@@ -4,181 +4,180 @@ var bingoList = {
 	"combined": "true",
 	"version": "v1.0"},
 "normal": {"1":  [
-{ name: "Hogwarts Chess Champion (5PC)" },
-{ name: "Learn Herbivicus (4PC)" },
-{ name: "10 000 Beans (2PC)" },
-{ name: "100 Potions in Inventory (2PC)" },
+{ "name": "Hogwarts Chess Champion (5PC)" },
+{ "name": "Learn Herbivicus (4PC)" },
+{ "name": "10 000 Beans (2PC)" },
+{ "name": "100 Potions in Inventory (2PC)" },
 ],
 
 "2": [
-{ name: "Unlock all Spells (7.2PC)" },
-{ name: "Learn to Apparate (7.2PC)" },
-{ name: "100% Streets of Hogsmeade (7.2PC)" },
-{ name: "All Music Tracks Collected (7.2PC)" },
+{ "name": "Unlock all Spells (7.2PC)" },
+{ "name": "Learn to Apparate (7.2PC)" },
+{ "name": "100% Streets of Hogsmeade (7.2PC)" },
+{ "name": "All Music Tracks Collected (7.2PC)" },
 ],
 
 "3": [
-{ name: "Perfect Score on a Quidditch Replay (6PC)" },
-{ name: "Beat 2 House Champions at Dueling (6PC)" },
-{ name: "50 Crests (6PC)" },
-{ name: "10 000 MiniCrests (6PC)" },
+{ "name": "Perfect Score on a Quidditch Replay (6PC)" },
+{ "name": "Beat 2 House Champions at Dueling (6PC)" },
+{ "name": "50 Crests (6PC)" },
+{ "name": "10 000 MiniCrests (6PC)" },
 ],
 
 "4": [
-{ name: "All 9 Missions (7.1PC)" },
-{ name: "All Deathly Hallow Symbols (7.1PC)" },
-{ name: "All Portraits (5PC)" },
-{ name: "Gold Moody’s Challenges (4PC)" },
+{ "name": "All 9 Missions (7.1PC)" },
+{ "name": "All Deathly Hallow Symbols (7.1PC)" },
+{ "name": "All Portraits (5PC)" },
+{ "name": "Gold Moody’s Challenges (4PC)" },
 ],
 
 "5": [
-{ name: "Master (4PC)" },
-{ name: "All Shields (4PC)" },
-{ name: "Clean the Castle (5PC)" },
-{ name: "Return Luna’s Items (5PC)" },
+{ "name": "Master (4PC)" },
+{ "name": "All Shields (4PC)" },
+{ "name": "Clean the Castle (5PC)" },
+{ "name": "Return Luna’s Items (5PC)" },
 ],
 
 "6": [
-{ name: "20 Bats with Buckbeak (3Xbox)" },
-{ name: "20 Stink Pellet Throws (3Xbox)" },
-{ name: "Complete Statue List (3Xbox)" },
-{ name: "Confiscated Items Room Empty (2PS2 ONLY)" },
+{ "name": "20 Bats with Buckbeak (3Xbox)" },
+{ "name": "20 Stink Pellet Throws (3Xbox)" },
+{ "name": "Complete Statue List (3Xbox)" },
+{ "name": "Confiscated Items Room Empty (2PS2 ONLY)" },
 ],
 
 "7": [
-{ name: "Kill 3 Venomous Tentacula (1Xbox)" },
-{ name: "Kill 7 Gytrashes (1Xbox/2Xbox)" },
-{ name: "All Shields in Spongify (1Xbox)" },
-{ name: "All Shields in Lumos (1Xbox)" },
+{ "name": "Kill 3 Venomous Tentacula (1Xbox)" },
+{ "name": "Kill 7 Gytrashes (1Xbox/2Xbox)" },
+{ "name": "All Shields in Spongify (1Xbox)" },
+{ "name": "All Shields in Lumos (1Xbox)" },
 ],
 
 "8": [
-{ name: "F&G Shop Sold Out (Non Replenishing Items) (Xbox/3PC)" },
-{ name: "Get Caught by Prefects in 4 Games (1GBA/2GBA/1Xbox/2Xbox/3Xbox)" },
-{ name: "Learn Diffindo 3 Times (1Xbox/2Xbox/2PC/3GBA)" },
-{ name: "Girding Potion in 2 Games (2GBA/3Xbox)" },
+{ "name": "F&G Shop Sold Out (Non Replenishing Items) (Xbox/3PC)" },
+{ "name": "Get Caught by Prefects in 4 Games (1GBA/2GBA/1Xbox/2Xbox/3Xbox)" },
+{ "name": "Learn Diffindo 3 Times (1Xbox/2Xbox/2PC/3GBA)" },
+{ "name": "Girding Potion in 2 Games (2GBA/3Xbox)" },
 ],
 
 "9": [
-{ name: "Beat Snake with Ron (2GBC)" },
-{ name: "Beat Lockhart in a Duel (2GBC)" },
-{ name: "Dragonskin Cloak (2GBC)" },
-{ name: "Discover 40 creatures with Informus (2GBC/3GBA)" },
+{ "name": "Beat Snake with Ron (2GBC)" },
+{ "name": "Beat Lockhart in a Duel (2GBC)" },
+{ "name": "Dragonskin Cloak (2GBC)" },
+{ "name": "Discover 40 creatures with Informus (2GBC/3GBA)" },
 ],
 
 "10": [
-{ name: "Defeat Voldemort Without EAS (1GBC)" },
-{ name: "Full Enchantment (1GBC)" },
-{ name: "Mastermind (1GBC)" },
-{ name: "50 Card Combos (GBC)" },
+{ "name": "Defeat Voldemort Without EAS (1GBC)" },
+{ "name": "Full Enchantment (1GBC)" },
+{ "name": "Mastermind (1GBC)" },
+{ "name": "50 Card Combos (GBC)" },
 ],
 
 "11": [
-{ name: "All 9 Yule Ball Levels (4GBA)" },
-{ name: "60 Cards (4GBA)" },
-{ name: "Complete All Blackboard Minigames (5GBA)" },
-{ name: "All Pumpkin Pasties (1GBA)" },
+{ "name": "All 9 Yule Ball Levels (4GBA)" },
+{ "name": "60 Cards (4GBA)" },
+{ "name": "Complete All Blackboard Minigames (5GBA)" },
+{ "name": "All Pumpkin Pasties (1GBA)" },
 ],
 
 "12": [
-{ name: "80 Red Beans (2GBA)" },
-{ name: "Defeat Lupin (3GBA)" },
-{ name: "26 Frogs (2GBA)" },
-{ name: "Open All Chests in Trading Club (3GBA)" },
+{ "name": "80 Red Beans (2GBA)" },
+{ "name": "Defeat Lupin (3GBA)" },
+{ "name": "26 Frogs (2GBA)" },
+{ "name": "Open All Chests in Trading Club (3GBA)" },
 ],
 
 "13": [
-{ name: "10 Lost and Found Quests (Xbox)" },
-{ name: "Skip Collecting 2 Spellbooks (2Xbox ONLY)" },
-{ name: "All 5 Gnome Tossings (2Xbox/2PS2)" },
-{ name: "Defeat 3 Types of Trolls (Forest/River/Mountain)" },
+{ "name": "10 Lost and Found Quests (Xbox)" },
+{ "name": "Skip Collecting 2 Spellbooks (2Xbox ONLY)" },
+{ "name": "All 5 Gnome Tossings (2Xbox/2PS2)" },
+{ "name": "Defeat 3 Types of Trolls (Forest/River/Mountain)" },
 ],
 
 "14": [
-{ name: "All gems in Gringotts (1PS1)" },
-{ name: "3 Fred & George Trades (1PS1)" },
-{ name: "Help Raymond find his Cat (1PS1)" },
-{ name: "Collect 22 Fireseeds (1PC/1PS1)" },
+{ "name": "All gems in Gringotts (1PS1)" },
+{ "name": "3 Fred & George Trades (1PS1)" },
+{ "name": "Help Raymond find his Cat (1PS1)" },
+{ "name": "Collect 22 Fireseeds (1PC/1PS1)" },
 ],
 
 "15": [
-{ name: "800+ Points from Gnomes Twice (2PS1)" },
-{ name: "Founders Duels (2PS1)" },
-{ name: "Beat Peeves 8 Times (1PC/2GBA/2PS1/2PC/3PC/3Xbox)" },
-{ name: "Get 100% during a round of 6 different Spell Lessons (1PC/2PS1)" },
+{ "name": "800+ Points from Gnomes Twice (2PS1)" },
+{ "name": "Founders Duels (2PS1)" },
+{ "name": "Beat Peeves 8 Times (1PC/2GBA/2PS1/2PC/3PC/3Xbox)" },
+{ "name": "Get 100% during a round of 6 different Spell Lessons (1PC/2PS1)" },
 ],
 
 "16": [
-{ name: "Requirements List (3PC)" },
-{ name: "400 Pumpkin Pasties (3PC)" },
-{ name: "100 Challenge Items (Stars+Shields) (1-3PC)" },
-{ name: "80 Secrets (2PC/3PC)" },
+{ "name": "Requirements List (3PC)" },
+{ "name": "400 Pumpkin Pasties (3PC)" },
+{ "name": "100 Challenge Items (Stars+Shields) (1-3PC)" },
+{ "name": "80 Secrets (2PC/3PC)" },
 ],
 
 "17": [
-{ name: "5 Fred & George Trades (1PC)" },
-{ name: "500 Beans (1PC)" },
-{ name: "Ron Gives Harry a Surprise in Bed (1PC/1GBA)" },
-{ name: "150 Wizard Cards" },
+{ "name": "5 Fred & George Trades (1PC)" },
+{ "name": "500 Beans (1PC)" },
+{ "name": "Ron Gives Harry a Surprise in Bed (1PC/1GBA)" },
+{ "name": "150 Wizard Cards" },
 ],
 
 "18": [
-{ name: "Defeat Voldemort in the Graveyard (HP4)" },
-{ name: "Sirius Dies (5PC)" },
-{ name: "Defeat Voldemort in Courtyard (HP7.2)" },
-{ name: "Aragog Dies (HP6)" },
+{ "name": "Defeat Voldemort in the Graveyard (HP4)" },
+{ "name": "Sirius Dies (5PC)" },
+{ "name": "Defeat Voldemort in Courtyard (HP7.2)" },
+{ "name": "Aragog Dies (HP6)" },
 ],
 
 "19": [
-{ name: "Learn Flipendo 5 Times (HP1)" },
-{ name: "Become Goyle 5 Times (HP2)" },
-{ name: "Fight Aragog 3 Times (HP2)" },
-{ name: "Hogwarts Dueling Champion Twice (2GBC/2PC)" },
+{ "name": "Learn Flipendo 5 Times (HP1)" },
+{ "name": "Become Goyle 5 Times (HP2)" },
+{ "name": "Fight Aragog 3 Times (HP2)" },
+{ "name": "Hogwarts Dueling Champion Twice (2GBC/2PC)" },
 ],
 
 "20": [
-{ name: "Win 2 Quidditch Cups by Playing (2PC/2Xbox/6PC/6DS)" },
-{ name: "Ministry of Magic in 3 Games (5PC/5GBA/7.1DS/7.1PC)" },
-{ name: "Watch Dobby Die Twice (7.1PC/7.1DS/7.2DS)" },
-{ name: "Duel Malfoy in 5 Games (2PC/2PS1/2GBC/5GBA/6PC/6DS)" },
+{ "name": "Win 2 Quidditch Cups by Playing (2PC/2Xbox/6PC/6DS)" },
+{ "name": "Ministry of Magic in 3 Games (5PC/5GBA/7.1DS/7.1PC)" },
+{ "name": "Watch Dobby Die Twice (7.1PC/7.1DS/7.2DS)" },
+{ "name": "Duel Malfoy in 5 Games (2PC/2PS1/2GBC/5GBA/6PC/6DS)" },
 ],
 
 "21": [
-{ name: "Learn About the Three Brothers Twice (HP7.1)" },
-{ name: "Get Caught by Filch in 3 Games (1GBC/1PC/1PS1/2GBA/2PS1)" },
-{ name: "Gobstones Champion (5PC/6DS)" },
-{ name: "Brew a Wiggenweld Potion in 3 Different Years (2GBA/2PC/6DS/6PC/7.1DS)" },
+{ "name": "Learn About the Three Brothers Twice (HP7.1)" },
+{ "name": "Get Caught by Filch in 3 Games (1GBC/1PC/1PS1/2GBA/2PS1)" },
+{ "name": "Gobstones Champion (5PC/6DS)" },
+{ "name": "Brew a Wiggenweld Potion in 3 Different Years (2GBA/2PC/6DS/6PC/7.1DS)" },
 ],
 
 "22": [
-{ name: "Skittles Champion (6DS)" },
-{ name: "16 Unique Potion Ingredients (6DS)" },
-{ name: "Become a Cat (7.2DS)	" },
-{ name: "Scabior Dies in Low Res (7.2DS)" },
+{ "name": "Skittles Champion (6DS)" },
+{ "name": "16 Unique Potion Ingredients (6DS)" },
+{ "name": "Become a Cat (7.2DS)	" },
+{ "name": "Scabior Dies in Low Res (7.2DS)" },
 ],
 
 "23": [
-{ name: "8 Potions Badges (6PC)" },
-{ name: "Upgrade All Spells to Max (7.1DS)" },
-{ name: "Destroy 4 Different Horcruxes (2GBC/2PC/2PS1/2Xbox/7.1DS/7.1PC/7.2DS/7.2PC)" },
-{ name: "300 Wizard Cards" },
+{ "name": "8 Potions Badges (6PC)" },
+{ "name": "Upgrade All Spells to Max (7.1DS)" },
+{ "name": "Destroy 4 Different Horcruxes (2GBC/2PC/2PS1/2Xbox/7.1DS/7.1PC/7.2DS/7.2PC)" },
+{ "name": "300 Wizard Cards" },
 ],
 
 "24": [
-{ name: "Harry Potter Wizard Card" },
-{ name: "500 Wizard Cards" },
-{ name: "Learn 20 Unique Spells" },
-{ name: "Equip Items to Every Slot for All 3 Companions (2GBC/3GBA)" },
+{ "name": "Harry Potter Wizard Card" },
+{ "name": "500 Wizard Cards" },
+{ "name": "Learn 20 Unique Spells" },
+{ "name": "Equip Items to Every Slot for All 3 Companions (2GBC/3GBA)" },
 ],
 
 "25": [
-{ name: "18 Health Increases" },
-{ name: "Meet Ghouls in 2 Different Years (2GBC/2PS1/3Xbox)" },
-{ name: "Collect 100 Chocolate Frogs from the World" },
-{ name: "Flipendo Duo in 3 Games (1GBC/2GBC/2PS1/3GBA)" },
+{ "name": "18 Health Increases" },
+{ "name": "Meet Ghouls in 2 Different Years (2GBC/2PS1/3Xbox)" },
+{ "name": "Collect 100 Chocolate Frogs from the World" },
+{ "name": "Flipendo Duo in 3 Games (1GBC/2GBC/2PS1/3GBA)" },
 ],
 "info": {
             "version": "v10.1"
         }
-}
-};
+}};
